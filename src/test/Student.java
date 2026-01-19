ef750f27cc2b2d00936d4840cd4e9a09f21c95a1
sivagamasrinivasan <784108@pdsb.net>
2026-01-19 15:48:23 -0500

    test version 2

---
+++ src/test/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author kumar
+ */
+public class Student {
+    private int sid;
+}
